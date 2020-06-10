e973b1c950c3915b76a38be005e24ce7bf78b841
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-10 16:38:50 +0530

    Ignore Node Modules

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const codecs = require('./codecs')
-const formats = require('./formats')
+const codecs = require("./codecs");
+const formats = require("./formats");
 
 module.exports = () => ({
-    name: 'video-transcoder-codecs-formats',
-    resolveId (source) {
-        if (source === 'codecs' || source === 'formats') return source
-        return null
-    },
-    load (id) {
-        if (id === 'codecs') return codecs
-        if (id === 'formats') return formats(codecs)
-        return null
-    }
-})
+  name: "video-transcoder-codecs-formats",
+  resolveId(source) {
+    if (source === "codecs" || source === "formats") return source;
+    return null;
+  },
+  load(id) {
+    if (id === "codecs") return JSON.stringify(codecs.CODEC_TYPES);
+    if (id === "formats") return JSON.stringify(formats(codecs));
+    return null;
+  },
+});
